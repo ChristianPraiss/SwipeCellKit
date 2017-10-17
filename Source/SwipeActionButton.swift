95fa491d9112435c6f4d0632f0bd5694ca506094
Christian Praiß <ChristianPraiss@users.noreply.github.com>
2017-10-17 17:37:33 +0200

    Update SwipeActionButton.swift

--- Source/SwipeActionButton.swift
+++ Source/SwipeActionButton.swift
@@ -85,6 +85,15 @@ class SwipeActionButton: UIButton {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        if let maskLayer = self.cornerMask as? CAShapeLayer {		
+ -            let maskPath = UIBezierPath(roundedRect: self.bounds,		
+ -                                        byRoundingCorners: [.bottomRight, .topRight],		
+ -                                        cornerRadii: CGSize(width: 0.0, height: 0.0))		
+ -            maskLayer.path = maskPath.cgPath		
+ -            maskLayer.frame = self.layer.bounds		
+ -            self.cornerMask = maskLayer		
+ -            self.layer.mask = maskLayer		
+ -        }
     }
 }
 
